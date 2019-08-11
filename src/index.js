33f6f2edcc4b35787b2ea0b81422b9ed8e22649f
Khushdeep Sidhu <khushdeep009@gmail.com>
2019-08-11 10:26:00 -0400

    Configured Socket.io

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const path = require ( 'path' )
+const http = require ( 'http' )
 const express = require ( 'express' )
+const socketio = require ( 'socket.io' )
 
 const app = express ()
 
 // configure port to make the application on heroku also
 const port = process.env.PORT || 3000
+
+// Create the HTTP server using the Express app
+const server = http.createServer( app )
+
+// Connect socket.io to the http server
+const io = socketio( server )
 
 // Define paths
 const publicDirPath = path.join ( __dirname, '../public' )
@@ -12,6 +20,11 @@ const publicDirPath = path.join ( __dirname, '../public' )
 // Setup static directory to serve
 app.use ( express.static ( publicDirPath ) )
 
-app.listen ( port, () => {
+// Listen for new connections to Socket.io
+io.on ( 'connection', () => {
+    console.log ( 'New WebSocket connection' )
+} )
+
+server.listen ( port, () => {
     console.log ( `Server listening on port ${port}` )
 } )
